242656eae4e5a28d40f27ccf8df34854432a250b
saeedlotfi <saeed@binnazabla.com>
2024-01-18 22:45:21 +0300

    Tags table created

---
+++ app/src/main/java/saeid/lotfi/samplenote/model/TagModel.kt
@@ -0,0 +1,12 @@
+package saeid.lotfi.samplenote.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class TagModel(
+    @PrimaryKey(autoGenerate = true)
+    val tagId: Long = 0,
+    val tagTitle: String,
+    val tagColor: Long
+)
